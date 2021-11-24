ef857c4e5ae074247f7c418576d740231a6568f1
Ahmed Faarih <faarih@Emptribunal.mv>
2021-11-24 13:56:59 +0500

    Fixed bug in Home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -143,11 +143,7 @@
                         <div class="form-group d-flex justify-content-center" data-aos="fade" data-aos-duration="150" data-aos-delay="350" data-aos-once="true">
                           <button class="btn login-btn" type="submit">LOGIN</button>
                         </div>
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
+
                     <div class="form-group row">
                         <div class="col-md-6 offset-md-4">
                             <div class="form-check">
